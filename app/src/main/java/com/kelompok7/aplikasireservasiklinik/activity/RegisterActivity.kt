6c62ebe4e2523a1286f1ae97c402e72b499db483
Kahfi Kurnia Aji <58323098+kahfikurniaaji@users.noreply.github.com>
2021-06-18 06:49:00 +0700

    Perubahan ke-0

---
+++ app/src/main/java/com/kelompok7/aplikasireservasiklinik/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.kelompok7.aplikasireservasiklinik.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kelompok7.aplikasireservasiklinik.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/kelompok7/aplikasireservasiklinik/model/Pasien.kt
@@ -0,0 +1,9 @@
+package com.kelompok7.aplikasireservasiklinik.model
+
+data class Pasien(
+    val nama: String,
+    val usia: Int,
+    val jenisKelamin:String,
+    val alamat:String,
+    val noHP:String
+)
